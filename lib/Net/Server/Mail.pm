17f9dec9027a0f852a27ffb4344594228b98c36c
Olivier Poitrey <rs@rhapsodyk.net>
2002-06-21 17:56:53 +0000

    first shoot

--- lib/Net/Server/Mail.pm
+++ lib/Net/Server/Mail.pm
@@ -2,8 +2,244 @@ package Net::Server::Mail;
 
 use 5.006;
 use strict;
+use Sys::Hostname;
+use IO::Select;
+use Carp;
 
 $Net::Server::Mail::VERSION = '0.01';
 
+sub new
+{
+    my($proto, @args) = @_;
+    my $class = ref $proto || $proto;
+    my $self  = {};
+    bless($self, $class);
+    return $self->init(@args);
+}
+
+sub init
+{
+    my $self = shift;
+    croak "odd number of arguments" if(@_ % 2);
+    my $options = $self->{options} =
+    {
+        handle_in           => undef,
+        handle_out          => undef,
+        socket              => undef,
+        error_sleep_time    => 0,
+        idle_timeout        => 0,
+    };
+    for(my $i = 0; $i < @_; $i += 2)
+    {
+        $options->{lc($_[$i])} = $_[$i + 1];
+    }
+
+    return unless
+    (
+        (defined $options->{handle_in} && defined $options->{handle_out})
+        || defined $options->{socket}
+    );
+
+    $self->reset_sender;
+    $self->reset_recipient;
+    $self->reset_data;
+
+    return $self;
+}
+
+sub set_cmd
+{
+    my($self, $cmd, $coderef) = @_;
+    $self->{cmd}->{uc $cmd} = $coderef;
+}
+
+sub del_cmd
+{
+    my($self, $cmd) = @_;
+    delete $self->{cmd}->{$cmd}
+        if defined $self->{cmd};
+}
+
+sub process
+{
+    my($self) = @_;
+
+    my $in  = $self->get_in;
+    my $sel = new IO::Select;
+    $sel->add($in);
+    
+    $self->banner;
+    while($sel->can_read($self->{options}->{idle_timeout} || undef))
+    {
+        $_ = <STDIN>;
+        chomp;
+        s/^\s+|\s+$//g;
+        next unless length;
+        my($cmd, @args) = split;
+        
+        if(exists $self->{cmd}->{uc $cmd})
+        {
+            my $rv = &{$self->{cmd}->{uc $cmd}}($self, @args);
+            # close connection if command return something
+            return $rv if(defined $rv);
+        }
+        else
+        {
+            $self->reply(502, 'Error: command not implemented');
+            next;
+        }
+    }
+
+    $self->timeout;
+}
+
+sub reply
+{
+    my($self, $code, $msg) = @_;
+    my $out = $self->get_out;
+    # tempo on error
+    sleep $self->{options}->{error_sleep_time}
+        if($code >= 400 && $self->{options}->{error_sleep_time});
+
+    $msg = $code >= 400 ? 'Failure' : 'Ok'
+        unless defined $msg;
+
+    my @lines = split(/\n\r?/, $msg);
+    for(my $i = 0; $i < @lines; $i++)
+    {
+        my $sep = $i == $#lines ? ' ' : '-';
+        print $out "$code$sep$lines[$i]\n\r";
+    }
+}
+
+sub get_in
+{
+    my($self) = @_;
+    if(defined $self->{options}->{handle_in})
+    {
+        return $self->{options}->{handle_in};
+    }
+    elsif(defined $self->{options}->{socket})
+    {
+        return $self->{options}->{socket};
+    }
+    else
+    {
+        croak "no handle to read";
+    }
+}
+
+sub get_out
+{
+    my($self) = @_;
+    if(defined $self->{options}->{handle_out})
+    {
+        return $self->{options}->{handle_out};
+    }
+    elsif(defined $self->{options}->{socket})
+    {
+        return $self->{options}->{socket};
+    }
+    else
+    {
+        croak "no handle to write";
+    }
+}
+
+sub get_hostname
+{
+    my($self) = @_;
+    return hostname;
+}
+
+sub get_protoname
+{
+    my($self) = @_;
+    return 'NOPROTO';
+}
+
+sub get_appname
+{
+    my($self) = @_;
+    return 'Net::Server::Mail (Perl)';
+}
+
+sub get_sender
+{
+    my($self) = @_;
+    return $self->{sender};
+}
+
+sub set_sender
+{
+    my($self, $sender) = @_;
+    $self->{sender} = $sender;
+}
+
+sub reset_sender
+{
+    my($self, $sender) = @_;
+    $self->{sender} = '';
+}
+
+sub get_recipient
+{
+    my($self) = @_;
+    return @{$self->{recipient}};
+}
+
+sub push_recipient
+{
+    my($self, @rcpt) = @_;
+    push @{$self->{recipient}}, @rcpt;
+}
+
+sub reset_recipient
+{
+    my($self) = @_;
+    $self->{recipient} = [];
+}
+
+sub get_data
+{
+    my($self) = @_;
+    return $self->{data};
+}
+
+sub put_data
+{
+    my($self, $data) = @_;
+    $self->{data} .= $data;
+}
+
+sub reset_data
+{
+    my($self, $date) = @_;
+    $self->{data} = '';
+}
+
+###########################################################
+
+sub banner
+{
+    my($self) = @_;
+    my $hostname  = $self->get_hostname  || '';
+    my $protoname = $self->get_protoname || '';
+    my $appname   = $self->get_appname   || '';
+    
+    my $str;
+    $str  = $hostname.' '  if length $hostname;
+    $str .= $protoname.' ' if length $protoname;
+    $str .= $appname       if length $appname;
+
+    $self->reply(220, $str);
+}
+
+sub timeout
+{
+    my($self) = @_;
+    $self->reply(421, 'Error: timeout exceeded');
+    return 1;
+}
 
 1;
--- lib/Net/Server/Mail/SMTP.pm
+++ lib/Net/Server/Mail/SMTP.pm
@@ -2,8 +2,280 @@ package Net::Server::Mail::SMTP;
 
 use 5.006;
 use strict;
+use base 'Net::Server::Mail';
 
-$Net::Server::Mail::VERSION = '0.01';
+sub init
+{
+    my($self, @args) = @_;
+    $self->SUPER::init(@args);
+
+    $self->set_cmd(HELO => \&helo);
+    $self->set_cmd(NOOP => \&noop);
+    $self->set_cmd(MAIL => \&mail);
+    $self->set_cmd(RCPT => \&rcpt);
+    $self->set_cmd(DATA => \&data);
+    $self->set_cmd(RSET => \&rset);
+    $self->set_cmd(QUIT => \&quit);
+
+    $self->set_callback(MAIL => \&mail_callback);
+
+    return $self;
+}
+
+sub get_protoname
+{   
+    my($self) = @_;
+    return 'SMTP';
+}
+
+
+sub helo
+{
+    my($self, $hostname) = @_;
+    unless(defined $hostname && length $hostname)
+    {
+        $self->reply(501, 'Syntax: HELO hostname');
+    }
+    $self->reply(250, $self->get_hostname);
+    return;
+}
+
+sub noop
+{
+    my($self) = @_;
+    $self->reply(250, 'Ok');
+    return;
+}
+
+=pod
+
+=head2 Callback MAIL
+
+    ($success, [$code, [$msg]]) = callback($address);
+
+=cut
+
+sub mail
+{
+    my($self, $from, $address, @options) = @_;
+
+    unless(defined $from && lc $from eq 'from:')
+    {
+        $self->reply(501, 'Syntax: MAIL FROM: <address>');
+        return;
+    }
+
+    if(length $self->get_sender)
+    {
+        $self->reply(503, 'Error: nested MAIL command');
+        return;
+    }
+
+    unless($self->mail_options)
+    {
+        return;
+    }
+
+    my($success, $code, $msg) = $self->callback('MAIL', $address);
+
+    if(defined $success && $success)
+    {
+        $self->set_sender($address);
+    }
+
+    if(defined $code)
+    {
+        $self->reply($code, $msg);
+    }
+    else
+    {
+        if(defined $success)
+        {
+            $self->reply(250, 'Ok');
+        }
+        else
+        {
+            $self->reply(550, 'Failure');
+        }
+    }
+
+    return;
+}
+
+sub mail_options
+{
+    my($self, @options) = @_;
+
+    if(@options)
+    {
+        $self->reply(555, "Unsupported option: $options[0]");
+        return 0;
+    }
+
+    return 1;
+}
+
+=pod
+
+=head2 Callback RCPT
+
+    ($success, [$code, [$msg]]) = callback($address);
+
+=cut
+
+
+sub rcpt
+{
+    my($self, $to, $address, @options) = @_;
+    unless(length $self->get_sender)
+    {
+        $self->reply(503, 'Error: need MAIL command');
+        return;
+    }
+    
+    unless(defined $to && lc $to eq 'to:')
+    {
+        $self->reply(501, 'Syntax: RCPT TO: <address>');
+        return;
+    }
+
+    unless($self->mail_options)
+    {
+        return;
+    }
+
+    my($success, $code, $msg) = $self->callback('RCPT', $address);
+
+    if(defined $success && $success)
+    {
+        $self->push_recipient($address);
+    }
+
+    if(defined $code)
+    {
+        $self->reply($code, $msg);
+    }
+    else
+    {
+        if(defined $success)
+        {
+            $self->reply(250, 'Ok');
+        }
+        else
+        {
+            $self->reply(550, 'Failure');
+        }
+    }
+
+    return;
+}
+
+sub rcpt_options
+{
+    my($self, @options) = @_;
+
+    if(@options)
+    {
+        $self->reply(555, "Unsupported option: $options[0]");
+        return 0;
+    }
+
+    return 1;
+}
+
+sub data
+{
+    my($self, @args) = @_;
+
+    unless($self->get_recipient)
+    {
+        $self->reply(503, 'Error: need RCPT command');
+        return;
+    }
+    
+    if(@args)
+    {
+        $self->reply(501, 'Syntax: DATA');
+        return;
+    }
+
+    $self->reply(354, 'End data with <CR><LF>.<CR><LF>');
+
+    my $in = $self->get_in;
+
+    while(<$in>)
+    {
+        last if(/^\.\n\r?$/);
+        
+        # RFC 821 compliance.
+        s/^\.\./\./;
+        $self->put_data($_);
+    }
+
+    return $self->data_finished;
+}
+
+sub data_finished
+{
+    my($self) = @_;
+
+    my $id = $self->queue;
+    $self->reply(250, "Ok queued as $id");
+    return;
+}
+
+sub rset
+{
+    my($self) = @_;
+    $self->reset_sender;
+    $self->reset_recipient;
+    $self->reset_data;
+    $self->reply(250, 'Ok');
+    return;
+}
+
+sub quit
+{
+    my($self) = @_;
+    $self->reply(221, 'Bye');
+    return 1; # close cnx
+}
+
+# queue mechanism, should be in Net::Server::Mail::Queue
+
+sub queue
+{
+    my($self) = @_;
+    
+    my $id = $self->generate_id;
+    
+    my $msg =
+    {
+        sender      => $self->get_sender,
+        recipient   => [$self->get_recipient],
+        data        => $self->get_data,
+    };  
+    
+    $self->reset_sender;
+    $self->reset_recipient;
+    $self->reset_data;
+    
+    $self->{queue}->{$id} = $msg;
+    
+    return $id;
+}   
+
+sub get_queue
+{
+    my($self) = @_;
+    return $self->{queue};
+}   
+
+sub generate_id
+{
+    my($self) = @_;
+    return(join('', map(('A'..'Z', 0..9)[int(rand(36))], 0..9)))
+}   
 
 
 1;
