f9be2ed28c74d8d5ffc42689897bec5f91464f5d
Olivier Poitrey <rs@rhapsodyk.net>
2002-06-25 12:52:22 +0000

    template class for ESMTP extension

---
+++ lib/Net/Server/Mail/ESMTP/Extension.pm
@@ -0,0 +1,38 @@
+package Net::Server::Mail::ESMTP::Extension;
+
+use 5.006;
+use strict;
+
+sub new
+{
+    my($proto) = @_;
+    my $class  = ref $proto || $proto;
+    my $self   = {};
+    bless($self, $class);
+    return $self;
+}
+
+sub verb
+{
+    return ();
+}
+
+sub keyword
+{
+    return 'XNOTOVERLOADED';
+}
+
+sub parameter
+{
+    return ();
+}
+
+sub option
+{
+    return ();
+}
+
+sub reply
+{
+    return ();
+}
