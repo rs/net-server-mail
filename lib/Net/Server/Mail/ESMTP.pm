8bccc7923ea2d1017baa0f4bfcf551b6c15220fd
Olivier Poitrey <rs@rhapsodyk.net>
2002-11-07 15:22:53 +0000

    Feed the documentation

--- lib/Net/Server/Mail/ESMTP.pm
+++ lib/Net/Server/Mail/ESMTP.pm
@@ -5,6 +5,79 @@ use strict;
 use Carp;
 use base qw(Net::Server::Mail::SMTP);
 
+=pod
+
+=head1 SYNOPSIS
+
+    use Net::Server::Mail::ESMTP;
+
+    my @local_domains = qw(example.com example.org);
+    my $server = new IO::Socket::INET Listen => 1, LocalPort => 25;
+
+    my $conn;
+    while($conn = $server->accept)
+    {
+        my $esmtp = new Net::Server::Mail::SMTP socket => $conn;
+        # activate some extensions
+        $esmtp->register('Net::Server::Mail::ESMTP::8BITMIME');
+        $esmtp->register('Net::Server::Mail::ESMTP::PIPELINING');
+        # adding some handlers
+        $esmtp->set_callback(RCPT => \&validate_recipient);
+        $esmtp->set_callback(DATA => \&queue_message);
+        $esmtp->process;
+    }
+
+    sub validate_recipient
+    {
+        my($session, $recipient) = @_;
+
+        my $domain;
+        if($recipient =~ /@(.*)>\s*$/)
+        {
+            $domain = $1;
+        }
+
+        if(not defined $domain)
+        {
+            return(0, 513, 'Syntax error.');
+        }
+        elsif(grep $domain eq $_, @local_domains)
+        {
+            return(0, 554, "$recipient: Recipient address rejected: Relay access denied");
+        }
+
+        return(1);
+    }
+
+    sub queue_message
+    {
+        my($session, $data) = @_;
+
+        my $sender = $session->get_sender();
+        my @recipients = $session->get_recipients();
+
+        return(0, 554, 'Error: no valid recipients')
+            unless(@recipients);
+
+        my $msgid = add_queue($sender, \@recipients, $data);
+          or return(0);
+
+        return(1, 250, "message queued $msgid");
+    }
+
+=head1 DESCRIPTION
+
+This class implement the ESMTP (RFC 2821) protocol.
+
+This class inherit from Net::Server::Mail::SMTP. Please see
+L<Net::Server::Mail::SMTP> for documentation of common methods.
+
+=head1 METHODS
+
+ESMTP specific methods.
+
+=cut
+
 sub init
 {
     my($self, @args) = @_;
@@ -28,6 +101,16 @@ sub get_extensions
     my($self) = @_;
     return(@{$self->{extensions} || []});
 }
+
+=pod
+
+=head2 register
+
+Activate an ESMTP extension. This method takes a module's name as
+argument. This module must implement certain methods. See
+L<Net::Server::Mail::ESMTP::Extension> for more details.
+
+=cut
 
 sub register
 {
@@ -56,7 +139,7 @@ sub register
     {
         $self->sub_reply(@$reply_def);
     }
-    
+
     push(@{$self->{extensions}}, $extend);
     return 1;
 }
@@ -78,6 +161,34 @@ sub sub_reply
         unless(grep($verb eq $_, $self->list_verb));
     push(@{$self->{xreply}->{$verb}}, $code);
 }
+
+=pod
+
+=head1 CALLBACKS
+
+Descriptions of callback who's can be used with set_callback
+method. All handle takes the Net::Server::Mail::ESMTP object as first
+argument and specific callback's arguments.
+
+=head2 EHLO
+
+Takes the hostname given as argument. Engage the reverse path step on
+success. RFC 2821 require thats EHLO command return the list of
+supported extension. Default success reply implement this, so it is
+deprecated to override this reply.
+
+You can rebuild extension list with get_extensions() method.
+
+Exemple:
+
+    my @extends;
+    foreach my $extend ($esmtp->get_extensions())
+    {
+        push(@extends, join(' ', $extend->keyword(), $extend->parameter()));
+    }
+    my $extends_string = join("\n", @extends);
+
+=cut
 
 sub ehlo
 {
@@ -103,7 +214,7 @@ sub ehlo
         arguments => [$hostname, \@extends],
         on_success => sub
         {
-            # according to the RFC, HELO ensures "that both the SMTP client
+            # according to the RFC, EHLO ensures "that both the SMTP client
             # and the SMTP server are in the initial state"
             $self->{extend_mode} = 1;
             $self->step_reverse_path(1);
@@ -139,7 +250,7 @@ sub handle_options
         $self->reply(555, "Unsupported option: $options[0]");
         return 0;
     }
-    
+
     for(my $i = $#options; $i >= 0; $i--)
     {
         my($key, $value) = split(/=/, $options[$i], 2);
@@ -172,5 +283,62 @@ sub handle_reply
 
     $self->reply($code, $msg);
 }
+
+=pod
+
+=head1 SEE ALSO
+
+Please, see L<Net::Server::Mail>, L<Net::Server::Mail::SMTP>
+and L<Net::Server::Mail::LMTP>.
+
+=head1 AUTHOR
+
+Olivier Poitrey E<lt>rs@rhapsodyk.netE<gt>
+
+=head1 AVAILABILITY
+
+The official FTP location is:
+
+B<ftp://ftp.rhapsodyk.net/pub/devel/perl/Net-Server-Mail/>
+
+Also available on CPAN.
+
+anonymous CVS repository:
+
+CVS_RSH=ssh cvs -d anonymous@cvs.rhapsodyk.net:/devel co
+Net-Server-Mail
+
+(supply an empty string as password)
+
+CVS repository on the web:
+
+http://www.rhapsodyk.net/cgi-bin/cvsweb/Net-Server-Mail/
+
+=head1 BUGS
+
+Please send bug-reports to rs-bugs@rhapsodyk.net.
+
+=head1 LICENCE
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 2.1 of the
+License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+
+=head1 COPYRIGHT
+
+Copyright (C) 2002 - Olivier Poitrey
+
+=cut
 
 1;
--- lib/Net/Server/Mail/LMTP.pm
+++ lib/Net/Server/Mail/LMTP.pm
@@ -4,6 +4,75 @@ use 5.006;
 use strict;
 use base qw(Net::Server::Mail::ESMTP);
 
+=pod
+
+=head1 SYNOPSIS
+
+    use Net::Server::Mail::ESMTP;
+
+    my @local_domains = qw(example.com example.org);
+    my $server = new IO::Socket::INET Listen => 1, LocalPort => 25;
+
+    my $conn;
+    while($conn = $server->accept)
+    {
+        my $esmtp = new Net::Server::Mail::SMTP socket => $conn;
+        # activate some extensions
+        $esmtp->register('Net::Server::Mail::ESMTP::8BITMIME');
+        $esmtp->register('Net::Server::Mail::ESMTP::PIPELINING');
+        # adding some handlers
+        $esmtp->set_callback(RCPT => \&validate_recipient);
+        $esmtp->set_callback(DATA => \&queue_message);
+        $esmtp->process;
+    }
+
+    sub validate_recipient
+    {
+        my($session, $recipient) = @_;
+
+        my $domain;
+        if($recipient =~ /@(.*)>\s*$/)
+        {
+            $domain = $1;
+        }
+
+        if(not defined $domain)
+        {
+            return(0, 513, 'Syntax error.');
+        }
+        elsif(grep $domain eq $_, @local_domains)
+        {
+            return(0, 554, "$recipient: Recipient address rejected: Relay access denied");
+        }
+
+        return(1);
+    }
+
+    sub queue_message
+    {
+        my($session, $data) = @_;
+
+        my $sender = $session->get_sender();
+        my @recipients = $session->get_recipients();
+
+        return(0, 554, 'Error: no valid recipients')
+            unless(@recipients);
+
+        my $msgid = add_queue($sender, \@recipients, $data);
+          or return(0);
+
+        return(1, 250, "message queued $msgid");
+    }
+
+=head1 DESCRIPTION
+
+This class implement the LMTP (RFC 2033) protocol.
+
+This class inherit from Net::Server::Mail::ESMTP. Please see
+L<Net::Server::Mail::ESMTP> for documentation of common methods.
+
+=cut
+
 sub init
 {
     my($self, @args) = @_;
@@ -18,9 +87,55 @@ sub init
     return $self;
 }
 
+=pod
+
+=head1 CALLBACKS
+
+Descriptions of callback who's can be used with set_callback
+method. All handle takes the Net::Server::Mail::ESMTP object as first
+argument and specific callback's arguments.
+
+=head2 LHLO
+
+Same as ESMTP EHLO, please see L<Net::Server::Mail::LMTP>.
+
+=cut
+
 sub lhlo
 {
-    shift->ehlo(@_);
+    my($self, $hostname) = @_;
+
+    unless(defined $hostname && length $hostname)
+    {
+        $self->reply(501, 'Syntax error in parameters or arguments');
+        return;
+    }
+
+    my $response = $self->get_hostname . ' Service ready';
+
+    my @extends;
+    foreach my $extend ($self->get_extensions)
+    {
+        push(@extends, join(' ', $extend->keyword, $extend->parameter));
+    }
+
+    $self->make_event
+    (
+        name => 'LHLO',
+        arguments => [$hostname, \@extends],
+        on_success => sub
+        {
+            # according to the RFC, LHLO ensures "that both the SMTP client
+            # and the SMTP server are in the initial state"
+            $self->{extend_mode} = 1;
+            $self->step_reverse_path(1);
+            $self->step_forward_path(0);
+            $self->step_maildata_path(0);
+        },
+        success_reply => [250, [$response, @extends]],
+    );
+
+    return;
 }
 
 sub data_finished
@@ -42,5 +157,62 @@ sub data_finished
 
     return;
 }
+
+=pod
+
+=head1 SEE ALSO
+
+Please, see L<Net::Server::Mail>, L<Net::Server::Mail::SMTP>
+and L<Net::Server::Mail::ESMTP>.
+
+=head1 AUTHOR
+
+Olivier Poitrey E<lt>rs@rhapsodyk.netE<gt>
+
+=head1 AVAILABILITY
+
+The official FTP location is:
+
+B<ftp://ftp.rhapsodyk.net/pub/devel/perl/Net-Server-Mail/>
+
+Also available on CPAN.
+
+anonymous CVS repository:
+
+CVS_RSH=ssh cvs -d anonymous@cvs.rhapsodyk.net:/devel co
+Net-Server-Mail
+
+(supply an empty string as password)
+
+CVS repository on the web:
+
+http://www.rhapsodyk.net/cgi-bin/cvsweb/Net-Server-Mail/
+
+=head1 BUGS
+
+Please send bug-reports to rs-bugs@rhapsodyk.net.
+
+=head1 LICENCE
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 2.1 of the
+License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+
+=head1 COPYRIGHT
+
+Copyright (C) 2002 - Olivier Poitrey
+
+=cut
 
 1;
