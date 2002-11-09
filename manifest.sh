f500b28e9b74c6730ae5624632a60180e2a2784c
Olivier Poitrey <rs@rhapsodyk.net>
2002-11-09 13:25:50 +0000

    bugfix

--- manifest.sh
+++ manifest.sh
@@ -1,1 +1,1 @@
-find -type f -a ! -path '*CVS*' -a ! -path './debian/*' -a ! -path './blib/*' -a ! -name pm_to_blib -a ! -name build-stamp -a ! -name manifest.sh -a ! -name '.*'|sed -e 's/^\.\///' > MANIFEST
+find README Changes Makefile.PL eg/ lib/ t/ -type f -a ! -path '*CVS*'|sed -e 's/^\.\///' > MANIFEST
