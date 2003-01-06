53deafd5b611173866b95d19ec5d3fc11d369cbf
Olivier Poitrey <rs@rhapsodyk.net>
2003-01-06 00:11:49 +0000

    mis-applied patch, sorry :)

--- lib/Net/Server/Mail.pm
+++ lib/Net/Server/Mail.pm
@@ -465,10 +465,11 @@ sub process
     $in->blocking(0);
     while($sel->can_read($self->{options}->{idle_timeout} || undef))
     {
+        $_ = join '', <$in>;
+        
         # do not go into an infinit loop if client close the connection
         last unless $_;
 
-        $_ = join '', <$in>;
         if(defined $self->next_input_to())
         {
             $self->tell_next_input_method($_);
