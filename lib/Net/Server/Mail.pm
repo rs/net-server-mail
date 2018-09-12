a39311372eb4fb5b987ee302a1d1a35fd319a2d6
andee <398680+andee@users.noreply.github.com>
2018-09-12 15:17:14 +0200

    when sysread say Resource temporarily unavailable
    Proposal how to fix bug: https://rt.cpan.org/Ticket/Display.html?id=127081

--- lib/Net/Server/Mail.pm
+++ lib/Net/Server/Mail.pm
@@ -7,6 +7,7 @@ use Sys::Hostname;
 use IO::Select;
 use IO::Handle;
 use Carp;
+use Errno;
 
 use constant HOSTNAME => hostname();
 
@@ -462,6 +463,9 @@ sub process {
         }
 
         if ( ( not defined $rv ) or ( $rv == 0 ) ) {
+
+            # No data available at the moment
+            next if not defined $rv and $!{'EAGAIN'};
 
             # read error or connection closed
             return $self->stop_session((not defined $rv) ? ($!) : ());
